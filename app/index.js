0edb0611dea7315f9c80a8b2e2d45ed71fa51461
Wan, Keely <kwan@mitre.org>
2019-07-25 19:01:03 -0400

    gameboard

--- app/index.js
+++ app/index.js
@@ -2,10 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 
+class GridBox extends React.Component {
+    render() {
+        return (
+            <div className="grid_box" style={{ backgroundColor: this.props.color }}>
+                {this.props.color}
+            </div>
+        )
+    }
+}
+
+class Gameboard extends React.Component {
+    render() {
+        return (
+            <div id="gameboard">
+                <GridBox color="#FBC9FF"></GridBox>
+                <GridBox color="#88E6D8"></GridBox>
+                <GridBox color="#FFD769"></GridBox>
+                <GridBox color="#C0F8B2"></GridBox>
+                <GridBox color="#A4B0F2"></GridBox>
+                <GridBox color="#FFC6C6"></GridBox>
+            </div>
+        )
+    }
+}
+
 class App extends React.Component {
     render() {
         return (
-            <div>Hello World</div>
+            <div id="container">
+                <Gameboard></Gameboard>
+            </div>
         )
     }
 }
